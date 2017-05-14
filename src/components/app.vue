a6e61f079d7f36eac8b893d085e5a51dc6dbc2b8
Tim Bellefleur <nomoon@phoebus.ca>
2017-05-14 02:11:59 -0700

    Initial VUE.js setup.

---
+++ src/components/app.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="app">
+  <div class="header">
+    <md-toolbar>
+      <h1 class="md-title">{{header}}</h1>
+    </md-toolbar>
+  </div>
+  <div class="page">
+    <router-view></router-view>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => {
+    return {
+      header: ''
+    }
+  }
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <md-layout md-gutter="24">
+    <span>{{text}}</span>
+  </md-layout>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data: () => {
+    return {
+      text: ''
+    }
+  }
+}
+</script>

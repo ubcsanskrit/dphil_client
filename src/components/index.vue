cf031d30fbcf74573356c2194ca13313573240a2
Tim Bellefleur <nomoon@phoebus.ca>
2017-05-14 04:29:03 -0700

    Add menu to Index

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,16 +1,23 @@
 <template>
-  <md-layout md-gutter="24">
-    <span>{{text}}</span>
+  <md-layout md-gutter md-align="center">
+    <md-layout md-flex="90">
+      <h2 class="md-title">Menu</h2>
+    </md-layout>
+    <md-layout md-flex="90">
+      <md-list style="flex: 1;">
+        <md-list-item>
+          <router-link to="tree">
+            <md-icon>call_split</md-icon>
+            <span>View Tree</span>
+          </router-link>
+        </md-list-item>
+      </md-list>
+    </md-layout>
   </md-layout>
 </template>
 
 <script>
 export default {
-  name: 'index',
-  data: () => {
-    return {
-      text: ''
-    }
-  }
+  name: 'index'
 }
 </script>

ac2553b68e28236632959537a090f322b404e267
Tim Bellefleur <nomoon@phoebus.ca>
2017-05-14 04:28:45 -0700

    Add Tree route component and Home button.

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,9 +1,14 @@
 <template>
 <div class="app">
   <div class="header">
-    <md-toolbar>
-      <h1 class="md-title">{{header}}</h1>
-    </md-toolbar>
+    <mv-layout mv-flex>
+      <md-toolbar style="flex: 1">
+        <h2 class="md-title" style="flex: 1;">{{header}}</h2>
+        <router-link to="/" class="md-button" :class="`md-theme-${$material.currentTheme}`">
+          <md-icon>home</md-icon>
+        </router-link>
+      </md-toolbar>
+    </mv-layout>
   </div>
   <div class="page">
     <router-view></router-view>
---
+++ src/components/tree.vue
@@ -0,0 +1,11 @@
+<template>
+  <md-layout md-flex>
+    <span>[Tree]</span>
+  </md-layout>
+</template>
+
+<script>
+export default {
+  name: 'tree'
+}
+</script>

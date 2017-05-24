98940b830a5f5115cd87847f2599fe2663d76ef0
Tim Bellefleur <nomoon@phoebus.ca>
2017-05-24 09:58:56 +0200

    Empty component template.

---
+++ src/components/empty.vue
@@ -0,0 +1,19 @@
+<template>
+<md-layout>
+
+</md-layout>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'empty',
+  computed: {}
+}
+</script>
